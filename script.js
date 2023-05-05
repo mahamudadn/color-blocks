fd3f768087aefeda26fedabac34b94fd7116bd38
Mahamud Aden <mahamudadn@gmail.com>
2023-05-04 19:47:09 -0500

    modified script

--- script.js
+++ script.js
@@ -1,40 +1,42 @@
 console.log('🟥 🟦 🟩 🟨');
 $(document).ready(onReady);
-// greated 
+// Crreated on ready file  
 function onReady(){
     console.log('We have JQuery in the home')
+
+// craeted an onclick that will add up.
 
     $('#red-btn').on('click', appendred)
     $('#blue-btn').on('click', appendBlue)
     $('#green-btn').on('click', appendgreen)
     $('#yellow-btn').on('click', appendyellow)
-    $('#blocks').on('click','.block', removeRed)
-    $('#blocks').on('click','.block', removeBlue)
-    $('#blocks').on('click','.block', removeGreen)
-    $('#blocks').on('click','.block', removeYellow)
 
-    
+// created an onclick function that will redmove what is clicked.
 
-
-
+    // $('#blocks').on('click','.block', removeRed)
+    // $('#blocks').on('click','.block', removeBlue)
+    // $('#blocks').on('click','.block', removeGreen)
+    // $('#blocks').on('click','.block', removeYellow)
 
 }
-
-let redCount = 1 ;
-let blueCount = 1 ; 
-let greenCount = 1 ;
-let yellowCount = 1 ; 
+// Defined counting values and gave them a valu of 1.
+    let redCount = 1 ;
+    let blueCount = 1 ; 
+    let greenCount = 1 ;
+    let yellowCount = 1 ; 
 
 
 function appendred() {
     $('#blocks').append('<div class="block red-fill"></div>')
-    redCount ++;
-    $('#red-count').html(redCount)
+    redCount ++;// adds 1 more to the value of redCount
+
+    // told JQeury to call redCount id and look it html.
+    $('#red-count').html(redCount) 
 }
 
 function appendBlue(){
     $('#blocks').append('<div class="block blue-fill"></div>')
-    blueCount ++;
+    blueCount ++; //adds 1 more to the value of blueCount
     $('#blue-count').html(blueCount)
 }
 
@@ -46,24 +48,24 @@ function appendgreen() {
 
 function appendyellow() {
     $('#blocks').append('<div class="block yellow-fill"></div>')
-    yellowCount ++;
+    yellowCount ++; //adds 1 more to the value of yellowCount
     $('#yellow-count').html(yellowCount)
 }
-
+// Removes Red color when you click on it in DOM.
 function removeRed(){
     $(this).remove()
     
 }
-
+// Removes Blue color when you click on it in DOM.
 function removeBlue(){
     $(this).remove()
     
 }
-
+//Removes Green color when you click on it in DOM.
 function removeGreen(){
     $(this).remove()
 }
-
+// Removes yellow color when you click on it in DOM.
 function removeYellow(){
     $(this).remove()
 }
